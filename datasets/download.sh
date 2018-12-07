f5f3605b82d5628dbe1c8c8a500cf4e2ec2357e5
Arunava <learningdroidarunava@gmail.com>
2018-12-08 00:02:36 +0530

    Added the dataset file

---
+++ datasets/download.sh
@@ -0,0 +1,4 @@
+echo "Getting the Malaria Dataset"
+echo "Starting Download..."
+wget --no-check-certificate https://www.kaggle.com/iarunava/cell-images-for-detecting-malaria/downloads/cell-images-for-detecting-malaria.zip/1
+echo "Dataset Downloaded Successfully!!"
