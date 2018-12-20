8bb86c05ae24b44482c7b0db3c6439ad1735e305
Arunava <learningdroidarunava@gmail.com>
2018-12-21 00:02:56 +0530

    Updated production code to infer using tensorflow and now using a frozen model

--- zeit/app/server.py
+++ zeit/app/server.py
@@ -5,6 +5,7 @@ from starlette.middleware.cors import CORSMiddleware
 import uvicorn, aiohttp, asyncio
 from io import BytesIO
 
+import tensorflow as tf
 import numpy as np
 import os
 import sys
@@ -26,21 +27,28 @@ app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Reques
 app.mount('/static', StaticFiles(directory='app/static'))
 
 async def download_file(url, dest):
+    print (1, dest)
     if dest.exists(): return
-    print (dest)
+    print (2, dest)
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             data = await response.read()
             with open(dest, 'wb') as f: f.write(data)
 
 async def setup_learner():
-    await download_file(model_file_url, path/'models'/f'{model_file_name}.hdf5')
-    model = load_model(str(path) + '/models/' + model_file_name + '.hdf5')
-    return model
+    await download_file(model_file_url, path/'models'/f'{model_file_name}.pb')
+    filepath = str(path) + '/models/' + model_file_name + '.pb'
+    with open(filepath, 'rb') as f:
+        content = f.read()
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(content)
+    with tf.Graph().as_default() as graph:
+        tf.import_graph_def(graph_def, name='')
+    return graph
 
 loop = asyncio.get_event_loop()
 tasks = [asyncio.ensure_future(setup_learner())]
-model = loop.run_until_complete(asyncio.gather(*tasks))[0]
+graph = loop.run_until_complete(asyncio.gather(*tasks))[0]
 loop.close()
 
 @app.route('/')
@@ -52,15 +60,17 @@ def index(request):
 async def analyze(request):
     data = await request.form()
     img_bytes = await (data['file'].read())
-    print (img_bytes, BytesIO(img_bytes))
-    #img = PIL.Image.open(BytesIO(img_bytes)).convert('RGB')
     img = plt.imread(BytesIO(img_bytes))
-    print (type(img))
     img = np.array(img)
     img = resize(img, (64, 64, 3))
-    img = img[np.newaxis, :]
-    predict = model.predict(img)
-    return JSONResponse({'result': str(classes[np.argmax(predict[0])])})
+
+    img = img[None, ...]
+    inp = graph.get_tensor_by_name('input_1_1:0')
+    out = graph.get_tensor_by_name('fc2_1/Softmax:0')
+
+    with tf.Session(graph=graph) as sess:
+        pred = sess.run([out], feed_dict={inp: img})
+    return JSONResponse({'result': str(classes[np.argmax(pred[0][0])])})
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app, host='0.0.0.0', port=5042)
